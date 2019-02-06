a11ea1dadc03dcda6ee7ce2a2f90b99a9d4244b1
Gustavo Kuze <gustavo.silva@rcadigital.com.br>
2019-02-06 11:37:44 -0200

    ColorButtons css

--- bilhetinhos/src/main/base/ColorPicker.jsx
+++ bilhetinhos/src/main/base/ColorPicker.jsx
@@ -1,15 +1,24 @@
+import './css/ColorButton.css'
 import React, { Component } from 'react'
 import materialColors from './js/NoteColors'
-import ColorButton from './ColorButton'
+
 
 export default class ColorPicker extends Component {
-
+    state = {
+        buttonsCheckState: []
+    }
+    
     colorButtons = Object.entries(materialColors).map(c => (
-        <div key={c[0]} className='col-1'>
-
-            <ColorButton colorName={c[0]} color={c[1]} />
+        <div key={c[0]} className='col-2-xs color-button-container' >
+            <input className="color-button-radio" id={`color-button-${c[0]}`} type="radio" value={c[1]} name="color" />
+            <label className=" color-button-label" for={`color-button-${c[0]}`}>
+                <span className="color-button-span d-flex justify-content-center" style={{ backgroundColor: c[1] }}>
+                    <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/242518/check-icn.svg" alt="Checked Icon" />
+                </span>
+            </label>
         </div>
     ))
+
 
     render() {
         return (
