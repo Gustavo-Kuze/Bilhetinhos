7ea549d9da76459a68b5181b58daa28470cda74a
Gustavo Kuze <gustavo.silva@rcadigital.com.br>
2019-02-06 13:48:47 -0200

    note accordion

--- bilhetinhos/src/main/base/ColorPicker.jsx
+++ bilhetinhos/src/main/base/ColorPicker.jsx
@@ -14,8 +14,8 @@ export default class ColorPicker extends Component {
 
     colorButtons = Object.entries(materialColors).map(c => (
         <div key={c[0]} className='col-2-xs color-button-container' >
-            <input className="color-button-radio" id={`color-button-${c[0]}`} type="radio" value={c[1]} name="color" onChange={this.handleChange}/>
-            <label className=" color-button-label" htmlFor={`color-button-${c[0]}`}>
+            <input className="color-button-radio" id={`color-button-${this.props.name}-${c[0]}`} type="radio" value={c[1]} name={this.props.name} onChange={this.handleChange} />
+            <label className=" color-button-label" htmlFor={`color-button-${this.props.name}-${c[0]}`}>
                 <span className="color-button-span d-flex justify-content-center align-items-center" style={{ backgroundColor: c[1] }}>
                     {/* <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/242518/check-icn.svg" alt="Checked Icon" /> */}
                     <i className="fas fa-check text-success"></i>
@@ -24,12 +24,15 @@ export default class ColorPicker extends Component {
         </div>
     ))
 
-
     render() {
         return (
             <div className='container'>
                 <div className='row'>
-                    {this.colorButtons}
+                    <div className="col-md-6">
+                        <div className="row">
+                            {this.colorButtons}
+                        </div>
+                    </div>
                 </div>
             </div>
         )
--- bilhetinhos/src/main/note/CreateNote.jsx
+++ bilhetinhos/src/main/note/CreateNote.jsx
@@ -1,18 +1,33 @@
-import React, { Component } from 'react'
-import Skeleton from '../base/Skeleton'
-import ColorPicker from '../base/ColorPicker'
+import React, { Component } from "react"
+import Skeleton from "../base/Skeleton"
+import ColorPicker from "../base/ColorPicker"
 
 export default class CreateNote extends Component {
     state = {
-        color: '#fffde7'
-    }
-    
-    handleColorChange = (e) => {
-        this.setState({...this.state, color: e.target.value})
+        noteColor: "#fffde7",
+        fontColor: "#333",
+        fontSize: 14,
+        message: ''
     }
 
-    colorChanged = color => {
-        this.setState({...this.state, color})
+    handleColorChange = (e) => {
+        this.setState({ ...this.state, color: e.target.value })
+    }
+
+    noteColorChanged = noteColor => {
+        this.setState({ ...this.state, noteColor })
+    }
+
+    fontColorChanged = fontColor => {
+        this.setState({ ...this.state, fontColor })
+    }
+
+    handleFontSizeChange = (e) => {
+        this.setState({ ...this.state, fontSize: parseInt(e.target.value) })
+    }
+
+    handleMessageChange = (e) => {
+        this.setState({ ...this.state, message: e.target.value })
     }
 
     render() {
@@ -24,14 +39,56 @@ export default class CreateNote extends Component {
                             <h1>Criar uma nota</h1>
                             <form>
                                 <div className="form-group ">
-                                    <p>Selecione uma cor:</p>
-                                    <ColorPicker colorChanged={this.colorChanged} />
-                                    <input type="color" name="color" value={this.state.color} onChange={this.handleColorChange} />
-                                    <p>Digite sua mensagem aqui:</p>
-                                    <textarea id="ta-note-message" className="form-control" name="note-message" rows="10" style={{backgroundColor: this.state.color}}></textarea>
+                                    <div className="accordion" id="note-options-accordion">
+                                        <div className="card">
+                                            <div className="card-header" id="accordion-heading-note-color">
+                                                <h2 className="mb-0">
+                                                    <button className="btn btn-link btn-lg btn-block text-left" type="button" data-toggle="collapse" data-target="#collapse-note-color" aria-expanded="false" aria-controls="collapse-note-color">
+                                                        Cor do bilhete
+                                             </button>
+                                                </h2>
+                                            </div>
+
+                                            <div id="collapse-note-color" className="collapse" aria-labelledby="accordion-heading-note-color" data-parent="#note-options-accordion">
+                                                <div className="card-body">
+                                                    <ColorPicker name="note-color" colorChanged={this.noteColorChanged} />
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div className="card">
+                                            <div className="card-header" id="accordion-heading-font-color">
+                                                <h2 className="mb-0">
+                                                    <button className="btn btn-link btn-lg btn-block text-left collapsed" type="button" data-toggle="collapse" data-target="#collapse-font-color" aria-expanded="false" aria-controls="collapse-font-color">
+                                                        Cor da fonte
+                                            </button>
+                                                </h2>
+                                            </div>
+                                            <div id="collapse-font-color" className="collapse" aria-labelledby="accordion-heading-font-color" data-parent="#note-options-accordion">
+                                                <div className="card-body">
+                                                    <ColorPicker name="font-color" colorChanged={this.fontColorChanged} />
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div className="card">
+                                            <div className="card-header" id="accordion-heading-font-size">
+                                                <h2 className="mb-0">
+                                                    <button className="btn btn-link btn-lg btn-block text-left collapsed" type="button" data-toggle="collapse" data-target="#collapse-font-size" aria-expanded="false" aria-controls="collapse-font-size">
+                                                        Tamanho da fonte
+                                                    </button>
+                                                </h2>
+                                            </div>
+                                            <div id="collapse-font-size" className="collapse" aria-labelledby="accordion-heading-font-size" data-parent="#note-options-accordion">
+                                                <div className="card-body">
+                                                    <input className="form-control" type="number" value={this.state.fontSize} onChange={this.handleFontSizeChange} />
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div><p>Digite sua mensagem aqui:</p>
+                                    <textarea id="ta-note-message" className="form-control" name="note-message" rows="10" onChange={this.handleMessageChange} style={{ backgroundColor: this.state.noteColor, color: this.state.fontColor, fontSize: this.state.fontSize }}></textarea>
                                     <button className="btn btn-primary btn-lg mt-3">Criar</button>
                                 </div>
                             </form>
+
                         </div>
                     </div>
 
