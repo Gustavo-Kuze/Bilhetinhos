9bc6396b5793107b4b07bf1a53f3dc42c16899b1
Gustavo Kuze <gustavo.silva@rcadigital.com.br>
2019-02-06 15:30:02 -0200

    Adicionada referencia ao Firebase (não npm)

---
+++ bilhetinhos/public/js/firebase.js
@@ -0,0 +1,9 @@
+var config = {
+    apiKey: "AIzaSyBGgOX_REap1yFQlTIlgPAErOqo66xbVbE",
+    authDomain: "projeto-teste-cbe9a.firebaseapp.com",
+    databaseURL: "https://projeto-teste-cbe9a.firebaseio.com",
+    projectId: "projeto-teste-cbe9a",
+    storageBucket: "projeto-teste-cbe9a.appspot.com",
+    messagingSenderId: "957489901010"
+};
+firebase.initializeApp(config);
