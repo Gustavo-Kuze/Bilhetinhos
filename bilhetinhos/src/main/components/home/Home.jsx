dba7dfd4a1590363495f309a88f03b3679cbb9d7
Gustavo-Kuze <gustavoksilva3@hotmail.com>
2019-02-07 19:08:20 -0200

    Criar usuário funcionando

--- bilhetinhos/src/main/components/home/Home.jsx
+++ bilhetinhos/src/main/components/home/Home.jsx
@@ -9,6 +9,8 @@ export default class Home extends Component {
                 <section id="home-banner" className="container-fluid text-center">
                     <h1>Início</h1>
                     <h2>Seja bem-vindo</h2>
+                    <a href="/user/signup" className="btn btn-primary">Sign Up</a>
+                    <a href="/user/login" className="btn btn-primary">Login</a>
                 </section>
             </Skeleton>
         )
--- bilhetinhos/src/main/components/user/SignUp.jsx
+++ bilhetinhos/src/main/components/user/SignUp.jsx
@@ -1,38 +1,81 @@
-import React, { Component } from 'react'
-import Skeleton from '../base/Skeleton'
+import firebase from "firebase";
+import React, { Component } from "react";
+import Skeleton from "../base/Skeleton";
 
 export default class SignUp extends Component {
+  state = {
+    email: "",
+    password: ""
+  };
 
-    signUp = (e) => {
-        e.preventDefault()
+  signUp = e => {
+    e.preventDefault();
 
-        
+    firebase
+      .auth()
+      .createUserWithEmailAndPassword(this.state.email, this.state.password)
+      .then(() => {
+          window.location.pathname = '/'
+      })
+      .catch(function(error) {
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        console.log('Ocorreu um erro ao tentar criar um usuário: ')
+        console.log(errorCode)
+        console.log(errorMessage)
+      });
 
-        return false
-    }
+    return false;
+  };
 
-    render() {
-        return (
-            <Skeleton>
-                <section className="container-fluid">
-                    <div className="row ">
-                        <div className="col-md-6 col-sm-10 offset-sm-1 offset-md-3 ">
-                            <h1 className="h3">Você está a poucos clique de se juntar a nós!</h1>
-                            <form className="mt-3" onSubmit={this.signUp}>
-                                <div className="form-group">
-                                    <label htmlFor="signup-email">E-mail</label>
-                                    <input id="signup-email" className="form-control" type="email" name="email" placeholder="Digite seu melhor E-mail aqui" required />
-                                </div>
-                                <div className="form-group">
-                                    <label htmlFor="signup-password">Senha</label>
-                                    <input id="signup-password" className="form-control" type="password" name="password" placeholder="Escolha uma senha forte" required />
-                                </div>
-                                <button className="btn btn-primary">Criar</button>
-                            </form>
-                        </div>
-                    </div>
-                </section>
-            </Skeleton>
-        )
-    }
+  handleEmailChanged = e => {
+    this.setState({ ...this.state, email: e.target.value });
+  };
+
+  handlePasswordChanged = e => {
+    this.setState({ ...this.state, password: e.target.value });
+  };
+
+  render() {
+    return (
+      <Skeleton>
+        <section className="container-fluid">
+          <div className="row ">
+            <div className="col-md-6 col-sm-10 offset-sm-1 offset-md-3 ">
+              <h1 className="h3">
+                Você está a poucos clique de se juntar a nós!
+              </h1>
+              <form className="mt-3" onSubmit={this.signUp}>
+                <div className="form-group">
+                  <label htmlFor="signup-email">E-mail</label>
+                  <input
+                    id="signup-email"
+                    className="form-control"
+                    type="email"
+                    name="email"
+                    placeholder="Digite seu melhor E-mail aqui"
+                    required
+                    onChange={this.handleEmailChanged}
+                  />
+                </div>
+                <div className="form-group">
+                  <label htmlFor="signup-password">Senha</label>
+                  <input
+                    id="signup-password"
+                    className="form-control"
+                    type="password"
+                    name="password"
+                    placeholder="Escolha uma senha forte"
+                    required
+                    onChange={this.handlePasswordChanged}
+                  />
+                </div>
+                <button className="btn btn-primary">Criar</button>
+              </form>
+            </div>
+          </div>
+        </section>
+      </Skeleton>
+    );
+  }
 }
