f7f9a81947cc47434bb09c599e51c4666238bd47
Gustavo Kuze <gustavo.silva@rcadigital.com.br>
2019-02-15 11:35:31 -0200

    Criada firebase-function registerUser

--- bilhetinhos/functions/index.js
+++ bilhetinhos/functions/index.js
@@ -1,14 +1,7 @@
 const functions = require('firebase-functions');
-
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
 const admin = require('firebase-admin');
-admin.initializeApp();
 
+admin.initializeApp();
 
 exports.getAllUsers = functions.https.onRequest((req, res) => {
     admin.database().ref('users/').once('value').then(usersSnapshot => {
@@ -19,25 +12,9 @@ exports.getAllUsers = functions.https.onRequest((req, res) => {
     })
 })
 
-// Take the text parameter passed to this HTTP endpoint and insert it into the
-// Realtime Database under the path /messages/:pushId/original
-// exports.addMessage = functions.https.onRequest((req, res) => {
-// Grab the text parameter.
-// const original = req.query.text;
-// Push the new message into the Realtime Database using the Firebase Admin SDK.
-// return admin.database().ref('/messages').push({original: original}).then((snapshot) => {
-// Redirect with 303 SEE OTHER to the URL of the pushed object in the Firebase console.
-//   return res.redirect(303, snapshot.ref.toString());
-// });
-//   });
-
-// exports.addMessage = functions.https.onRequest((req, res) => {
-//     const uid = req.body.uid;
-//     const original = req.query.text;
-
-    
-//     return admin.database().ref('/messages').push({ original: original }).then((snapshot) => {
-
-//         return res.redirect(303, snapshot.ref.toString());
-//     });
-// });
+exports.registerUser = functions.https.onRequest((req, res) => {
+    admin.database().ref(`users`).child(req.body.uid).set(req.body).then(() => {
+        res.json(req.body)
+        return req.body
+    }).catch(err => res.json(err))
+})
